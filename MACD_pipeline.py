03dbe2ecf92b93130c7f59d4160b49725c086f18
Amit <amit@Amits-MBP.fios-router.home>
2020-04-16 22:17:09 -0400

    MACD pipeline implemented

--- MACD_pipeline.py
+++ MACD_pipeline.py
@@ -1,10 +1,12 @@
 import math
 import time
+from datetime import datetime
 
 import pandas as pd
 import shift
 from TraderS import TraderS
 import logging as lg
+from numpy import nan
 
 
 from MACD_config import fast, slow, signal_period
@@ -13,10 +15,13 @@ class MACD_pipeline:
 
     def __init__(self,tickers):
         self.tickers = tickers
-        self.df_columns = [ 'TICKER', 'LAG', 'CURRENT_PRICE','EMA_SLOW', 'EMA_FAST', 'MACD','SIGNAL_LINE','TRADE_SIGNAL','TRADE_DECISION']
-        self.df = pd.DataFrame(columns=self.df_columns)
-        self.df.TICKER = tickers
-        lg.debug(self.df)
+        self.df_columns = [ 'TIME', 'TICKER', 'CURRENT_PRICE','EMA_SLOW', 'EMA_FAST', 'MACD','SIGNAL_LINE','TRADE_SIGNAL','TRADE_DECISION']
+        #self.df_pre = pd.DataFrame(columns=self.df_columns)
+        self.df_current = pd.DataFrame(columns=self.df_columns)
+        self.df_current.TICKER = tickers
+        self.df_current.TIME = now = datetime.now().time()
+
+        lg.debug(self.df_current)
 
     def get_current_price(self):
         current_prices = {}
@@ -25,62 +30,64 @@ class MACD_pipeline:
             current_price = trader.get_last_price(ticker)
             current_prices[ticker] = current_price
             self.update_ticker(ticker, current_price)
+            df = pd.DataFrame(self.current_data, index=[0])
+            self.df_current = pd.concat([self.df_current, df])
+        lg.debug(self.df_current.head(10))
         lg.debug(current_prices)
         return current_prices
 
-    def ema(self,ticker ,period, current_price, ema_type):
+    def ema(self,period, current_price, ema_val):
         beta = 1/(period + 1)
-        pre_ema = self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), ema_type]
         first_ema = False
 
         #update MACD values
-        if not pre_ema.dropna().empty:
-            previous_ema = pre_ema.values[0]
+        if ema_val and not math.isnan(ema_val):
+            previous_ema = ema_val
             current_ema = beta*current_price + (1-beta)*previous_ema
             lg.debug('current value {0} pre_ema {1}'.format(current_price,previous_ema))
         else:
             first_ema = True
             current_ema = current_price
-            self.df.loc[(self.df['TICKER'] == ticker) , 'LAG'] = 0
-            self.df.loc[(self.df['TICKER'] == ticker), 'CURRENT_PRICE'] = current_price
-        self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), ema_type] = current_ema
-        if not pre_ema.dropna().empty:
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 1), ema_type] = previous_ema
         return first_ema, current_ema
 
-    def signal_line(self,ticker ,period, current_macd, ema_type):
-        beta = 1/(signal_period + 1)
-        first_signal ,signal_line = self.ema(ticker, signal_period, current_macd, 'SIGNAL_LINE')
+    def signal_line(self ,period, current_macd, last_record):
+        sig_line = last_record.get('SIGNAL_LINE')
+        first_signal ,signal_line = self.ema(period, current_macd, sig_line)
         if first_signal:
+            self.current_data['SIGNAL_LINE'] = current_macd
             return
         trade_signal = 0
+        self.current_data['SIGNAL_LINE'] = signal_line
         if signal_line > current_macd:
             trade_signal = -1
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'TRADE_SIGNAL'] = -1
+            self.current_data['TRADE_SIGNAL'] = -1
         elif signal_line < current_macd:
             trade_signal = 1
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'TRADE_SIGNAL'] = 1
-        self.trade_decision(trade_signal,ticker)
+            self.current_data['TRADE_SIGNAL'] = 1
+        self.trade_decision(trade_signal,last_record)
 
-    def trade_decision(self,current_trade_signal,ticker):
+    def trade_decision(self,current_trade_signal,last_record):
+        ticker = last_record.get('TICKER')
         beta = 1/(signal_period + 1)
-        ts_p = self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'TRADE_SIGNAL']
-        previous_trade_signal = ts_p.values[0]
+        previous_trade_signal = last_record.get('TRADE_SIGNAL')
+        if math.isnan(previous_trade_signal):
+            self.current_data['TRADE_SIGNAL'] = current_trade_signal
+            return
         if current_trade_signal > previous_trade_signal:
             # Buy the stocks
             ticker_buy = shift.Order(shift.Order.Type.MARKET_BUY, ticker, 20)
-            TraderS.getInstance.submit_order(ticker_buy)
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'TRADE_DECISION'] = 1
+            TraderS.getInstance().submit_order(ticker_buy)
+            self.current_data['TRADE_DECISION'] = 1
             lg.debug('Buy the stock {0}'.format(ticker))
         elif current_trade_signal < previous_trade_signal:
             # Sell the stocks
             ticker_sell = shift.Order(shift.Order.Type.MARKET_SELL, ticker, 20)
-            TraderS.getInstance.submit_order(ticker_sell)
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'TRADE_DECISION'] = -1
+            TraderS.getInstance().submit_order(ticker_sell)
+            self.current_data['TRADE_DECISION'] = -1
             lg.debug('Sell the stock {0}'.format(ticker))
         else:
             lg.debug('No Buy Sell')
-        lg.debug(self.df)
+        lg.debug(self.df_current)
 
 
     def schedule_macd(self):
@@ -88,27 +95,40 @@ class MACD_pipeline:
         while(True):
             self.get_current_price()
             time.sleep(1)
+            self.df_current.drop(self.df_current[self.df_current['Age'] < 25].index, inplace=True)
 
     def trader_disconnect(self):
         TraderS.disconnect()
 
-    def update_ticker(self,ticker, current_price):
-        first_ema , current_slow_ema = self.ema(ticker,fast, current_price, 'EMA_SLOW')
-        first_ema ,current_fast_ema = self.ema(ticker,slow, current_price, 'EMA_FAST')
+    def update_ticker(self, ticker, current_price):
+        self.current_data = {'TIME': datetime.now().time() , 'TICKER': '', 'CURRENT_PRICE': nan,'EMA_SLOW' : nan, 'EMA_FAST': nan, 'MACD': nan,'SIGNAL_LINE': nan,'TRADE_SIGNAL': nan,'TRADE_DECISION': nan}
+        self.current_data['CURRENT_PRICE'] = current_price
+        self.current_data['TICKER'] = ticker
+        tic = self.df_current.loc[(self.df_current['TICKER'] == ticker)]
+        last_record = self.df_current.loc[(self.df_current['TICKER'] == ticker)].sort_values(by=['TIME'], ascending=False).head(1)
+        last_record = last_record.iloc[0]
+        pre_slow_ema = last_record.get('EMA_SLOW')
+        first_ema, current_slow_ema = self.ema(slow, current_price, pre_slow_ema)
+        self.current_data['EMA_SLOW'] = current_slow_ema
         if first_ema:
             return
+        pre_fast_ema = last_record.get('EMA_FAST')
+        first_ema, current_fast_ema = self.ema(fast, current_price, pre_fast_ema)
+        self.current_data['EMA_FAST'] = current_slow_ema
+        if first_ema:
+            return
+
         macd = current_fast_ema - current_slow_ema
-        y1 = self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'MACD']
-        previous_macd = self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'MACD'].values[0]
-        self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 0), 'MACD'] = macd
-        x = previous_macd and not math.isnan(previous_macd)
-        lg.debug('previous macd {}'.format(x))
-        if x:
-            self.df.loc[(self.df['TICKER'] == ticker) & (self.df['LAG'] == 1), 'MACD'] = previous_macd
-            current_trade_signal = self.signal_line(ticker,signal_period,macd,'SIGNAL_LINE')
+        self.current_data['MACD'] = macd
+        previous_macd = last_record.get('MACD')
+        lg.debug('previous macd {}'.format(previous_macd))
+        if previous_macd:
+            current_trade_signal = self.signal_line(signal_period,macd,last_record)
 
 
 # Run the macd code
 #mscd = MACD_pipeline(['SPY', 'VIXY', 'DIA', 'AAPL'])
+#TraderS.getInstance().sub_all_order_book()
+#time.sleep(5)
 #mscd.schedule_macd()
 
